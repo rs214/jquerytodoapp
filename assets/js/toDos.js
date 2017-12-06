9584a80ccaa45e778331cf1cf06cba408303a7ad
Robert Si <robertsi0214@gmail.com>
2017-12-05 17:55:44 -0800

    complete basic functionality of front end of jQuery todo list

--- assets/js/toDos.js
+++ assets/js/toDos.js
@@ -1,14 +1,22 @@
 //Check off specific todos while clicking
 
-$("li").on("click", function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
 
 //Click on X to delete todo
 
-$("span").on("click", function(e) {
+$("span").on("click", "X", function(e) {
   $(this).parent().fadeOut(500, function() {
     $(this).remove();
   });
   e.stopPropagation();
 })
+
+$("input[type='text']").keypress(function(e) {
+   if (e.which === 13) {
+    var toDoText = $(this).val();
+    $(this).val("");
+    $("ul").append(`<li><span>X </span>${toDoText}</li>`);
+   }
+});
